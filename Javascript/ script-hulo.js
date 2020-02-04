a14a20bc3ca377e4469051ee700bae5baab15d9a
abdallahalkhatatbeh <alkhatatbehabd@gmail.com>
2020-02-04 22:35:51 +0200

    Added new things

---
+++ Javascript/ script-hulo.js
@@ -0,0 +1,10 @@
+var mark = prompt("Please enter your mark" );
+
+if (mark >= 90) {
+ alert("exellent, pass!") ;
+ } else if (mark >= 50) {
+alert( "good, pass!");
+ }
+  else {
+ alert("Sadly you faild,Better luck next time");
+ } 
