4c59a752d5a128af2343d3e5891c8798ea5539c7
abdallahalkhatatbeh <alkhatatbehabd@gmail.com>
2020-02-05 21:30:31 +0200

    Added a repetive function

--- Javascript/ script-hulo.js
+++ Javascript/ script-hulo.js
@@ -1,19 +1,46 @@
 
 
-function grades(){
-var mark = prompt("Please enter your mark" );
+// function grades(){
+// var mark = prompt("Please enter your mark" );
 
-if (mark >= 90) {
- alert("exellent, pass!") ;
- } else if (mark >= 50) {
-alert( "good, pass!");
- }
-  else {
- alert("Sadly you faild,Better luck next time");
+// if (mark >= 90) {
+//  alert("exellent, pass!") ;
+//  } else if (mark >= 50) {
+// alert( "good, pass!");
+//  }
+//   else {
+//  alert("Sadly you faild,Better luck next time");
 
- }
+//  }
 
 
-}
+// }
 
-grades();
+//grades();
+
+function cofeeAndTea (){
+    var  order = prompt ("what do you want to order ? (coffee or tea)");
+    while (order !== 'tea' && order !== 'coffee') {
+        order = prompt ("what do you want to order ? (coffee or tea)");
+    }
+    var noOfOrders = prompt ("how many cups do you want ?") ;
+    
+    var item = "";
+    
+    for(var i=0 ; i< noOfOrders; i++)
+    if (order==="tea")
+     {
+         item =item+ '<img src="Tea.jpeg">' ;
+     }
+     else if (order=== "coffee")
+     {
+        item =item+ '<img src="cofee.jpg">' ; 
+     } else {
+        item = '<strong> Sorry we dont sereve that here </strong>' ; 
+    
+     }
+     return item ;
+    }
+    document.write('<h3>'+cofeeAndTea()+'</h3>');
+    
+    
